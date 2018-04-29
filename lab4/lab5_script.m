73e2b457fb40597b9c22a9f377610798c1540d6d
Yurij Lezhenin <yurij.lezhenin@yandex.ru>
2018-04-29 21:35:31 +0300

    finish lab 4-5

--- lab4/lab5_script.m
+++ lab4/lab5_script.m
@@ -16,11 +16,11 @@ f = (-N/2:N/2-1) .* (Fs/N);
 
 x = cos(2 * pi * F0 * t);
 
-y_pmod = pmmod(x, Fc, Fs, 2 * pi / 3);
-y_fmod = fmmod(x, Fc, Fs, 8);
+y_pmod = pmmod(x, Fc, Fs, 5 * pi / 6);
+y_fmod = fmmod(x, Fc, Fs, 12);
 
-y_pdemod = pmdemod(y_pmod, Fc, Fs, 2 * pi / 3);
-y_fdemod = pmdemod(y_fmod, Fc, Fs, 8);
+y_pdemod = pmdemod(y_pmod, Fc, Fs, 5 * pi / 6);
+y_fdemod = fmdemod(y_fmod, Fc, Fs, 12);
 
 
 s = fftshift(fft(x));
@@ -34,38 +34,47 @@ figure(figure_properties{:})
 hold on
 plot(t, y_pmod, 'r', 'LineWidth', lw)
 plot(t, x, 'b', 'LineWidth', lw)
+set(gca, 'FontSize', fs)
 
 figure(figure_properties{:})
 hold on
+plot(f, abs(s), 'b', 'LineWidth', lw)
 plot(f, abs(s_pmod), 'r', 'LineWidth', lw)
-xlim([-Fs/2, Fs/2]);
+xlim([-Fc*2, Fc*2]);
+set(gca, 'FontSize', fs)
 
 figure(figure_properties{:})
 hold on
 plot(t, y_fmod, 'r', 'LineWidth', lw)
 plot(t, x, 'b', 'LineWidth', lw)
+set(gca, 'FontSize', fs)
 
 figure(figure_properties{:})
 hold on
+plot(f, abs(s), 'b', 'LineWidth', lw)
 plot(f, abs(s_fmod), 'r', 'LineWidth', lw)
-xlim([-Fs/2, Fs/2]);
+xlim([-Fc*2, Fc*2]);
+set(gca, 'FontSize', fs)
 
 figure(figure_properties{:})
 hold on
 plot(t, y_pdemod, 'b', 'LineWidth', lw)
 %plot(t, x, 'b', 'LineWidth', lw)
+set(gca, 'FontSize', fs)
 
-figure(figure_properties{:})
-hold on
-plot(f, abs(s_pdemod), 'b', 'LineWidth', lw)
-xlim([-Fs/2, Fs/2]);
+% figure(figure_properties{:})
+% hold on
+% plot(f, abs(s_pdemod), 'b', 'LineWidth', lw)
+% xlim([-Fc*2, Fc*2]);
 
 figure(figure_properties{:})
 hold on
 plot(t, y_fdemod, 'b', 'LineWidth', lw)
 %plot(t, x, 'b', 'LineWidth', lw)
+set(gca, 'FontSize', fs)
 
-figure(figure_properties{:})
-hold on
-plot(f, abs(s_fdemod), 'b', 'LineWidth', lw)
-xlim([-Fs/2, Fs/2]);
+% figure(figure_properties{:})
+% hold on
+% plot(f, abs(s_fdemod), 'b', 'LineWidth', lw)
+% xlim([-Fc*2, Fc*2]);
+
